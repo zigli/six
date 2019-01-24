19ee9712cb68478d9b02bf89de25fd9e4547a8a6
ilgiz <4ilgiz@gmail.com>
2019-01-24 15:02:32 +0300

    Область видимости. Переменные x,y.222

--- src/cf/tilgiz/Main.java
+++ src/cf/tilgiz/Main.java
@@ -2,13 +2,14 @@ package cf.tilgiz;
 
 public class Main {
 
-    int x;
+    static int z;
 
     public static void main(String[] args) {
         int y = 0;
         Shape circle = new Circle();
         circle.draw();
         System.out.println("y = " + y);
+        System.out.println("z = " + z);
     }
 
 }
